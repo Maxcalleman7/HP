c73cf196a8a137addc9985be3e53687795f4b67e
Cale_exe <82705349+Maxcalleman7@users.noreply.github.com>
2026-05-19 12:04:24 +0200

    Add button IDs to HTML and implement character data in JavaScript

---
+++ javascript.js
@@ -0,0 +1,24 @@
+const visaAllButton = document.getElementById('visa-alla');
+const slytherinButton = document.getElementById('slytherin');
+const gryffindorButton = document.getElementById('gryffindor');
+
+const Characters = [
+
+    {id: 1 , name: 'Harry Potter', house: 'Gryffindor', image: 'Bilder/HarryPotter.png'},
+    {id: 2 , name: 'Hermione Granger', house: 'Gryffindor', image: 'Bilder/HermioneGranger.png'},
+    {id: 3 , name: 'Ron Weasley', house: 'Gryffindor', image: 'Bilder/RonWeasley.png'},
+    {id: 4 , name: 'Draco Malfoy', house: 'Slytherin', image: 'Bilder/DracoMalfoy.png'},
+    {id: 5 , name: 'Severus Snape', house: 'Slytherin', image: 'Bilder/SeverusSnape.png'},
+]
+
+function displayCharacters(characters) {
+
+
+
+}
+
+visaAllButton.addEventListener('click', () => {
+    console.log('Visa alla knappen klickad');
+});  
+
+
